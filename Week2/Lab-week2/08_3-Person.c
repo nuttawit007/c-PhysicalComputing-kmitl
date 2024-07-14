ef5bf71e9186aa8203691ef6cb6f4ef2be2740e0
WIN <nuttawit.ph@gmail.com>
2024-07-14 12:26:59 +0700

    Update : week2 lab2 08

--- Week2/Lab-week2/08_3-Person.c
+++ Week2/Lab-week2/08_3-Person.c
@@ -1,18 +1,20 @@
-// #include <stdio.h>
+// วิธี 1
+#include <stdio.h>
 
-// int main() {
-//     char fname1[31], sname1[31], person2[61], person3[61];
-//     scanf("%s", fname1);
-//     scanf("%s\n", sname1);
-//     scanf("%[^\n]\n", person2);
-//     scanf("%[^\n]", person3);
+int main() {
+    char fname1[31], sname1[31], person2[61], person3[61];
+    scanf("%s", fname1);
+    scanf("%s\n", sname1);
+    scanf("%[^\n]\n", person2);
+    scanf("%[^\n]", person3);
 
-//     printf("Person 1: %s %s\n", fname1, sname1);
-//     printf("Person 2: %s\n", person2);
-//     printf("Person 3: %s", person3);
-//     return 0;
-// }
+    printf("Person 1: %s %s\n", fname1, sname1);
+    printf("Person 2: %s\n", person2);
+    printf("Person 3: %s", person3);
+    return 0;
+}
 
+// วิธี 2
 #include<stdio.h>
  
 int main() {
