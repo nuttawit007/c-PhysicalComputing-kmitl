a63f16b84cfe9e383f6df5efe88bfca0d28d1113
WIN <nuttawit.ph@gmail.com>
2024-07-18 14:50:57 +0700

    Add : pre-condition

---
+++ Pre/Pre_Condition.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main() {
+    double distance;
+    scanf("%lf", &distance);
+
+    // if (distance > 52.9 && distance <= 58.855) {
+    //     printf("Chon Buri");
+    // } else if (distance > 35.477) {
+    //     printf("Chachoengsao");
+    // } else if (distance > 5.032) {
+    //     printf("Samut Prakarn");
+    // } else if (distance >= 0) {
+    //     printf("Bangkok");
+    // } else {
+    //     printf("InValid");
+    // }
+
+    if (distance > 58.855 || distance < 0) {
+        printf("InValid");
+    } else if (distance >= 0 && distance <= 5.032) {
+        printf("Bangkok");
+    } else if (distance > 5.032 && distance <= 35.477) {
+        printf("Samut Prakarn");
+    } else if (distance > 35.477 && distance <= 52.9) {
+        printf("Chachoengsao");
+    } else if (distance > 52.9 && distance <= 58.855){
+        printf("Chon Buri");
+    };
+
+    return 0;
+
+}
