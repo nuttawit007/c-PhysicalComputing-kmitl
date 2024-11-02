050fbc465d4e4cb80aa5838726893acedf65dd93
WIN <nuttawit.ph@gmail.com>
2024-11-02 13:40:36 +0700

    add : lab  week 9

--- Week9/Lab09/04_String Collector.c
+++ Week9/Lab09/04_String Collector.c
@@ -2,12 +2,34 @@
 #include <stdlib.h>
 
 int main() {
-    char letter;
-    char *str;
+    char *str, word;
+    int size = 0, cap = 10;
+
+    str = (char*) malloc(sizeof(char) * cap);
 
     while (1) {
-        scanf("%c", &letter);     // Read an char from the user
-        if (letter == '-')          // Stop if the user enters -
+        scanf(" %c", &word);
+        if (word == '-') {
             break;
+        }
+        if (size >= cap) {
+            cap *= 2;
+            char *temp = (char*) realloc(str, sizeof(char) * cap);
+            str = temp;
+        }
+
+        *(str + size++) = word;
     }
+    char *ptr = str;
+    while (ptr < str + size) {
+        printf("%c", *ptr++);
+    }
+    printf("\n");
+    while (ptr > str) {
+        printf("%c", *--ptr);
+    }
+    printf("\n");
+    free(str);
+    return 0;
+    
 }
